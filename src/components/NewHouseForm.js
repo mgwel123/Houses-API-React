6f5fbce199d68233d33cc8459af5fc2f9ab61bf0
Michael Welker <mikegw224@gmail.com>
2022-05-13 16:34:22 -0700

    adding New House Form

---
+++ src/components/NewHouseForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const NewHouseForm = (props) => {
+    const [ name, setName] = useState('');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (name) {
+            
+        }
+    }
+}
